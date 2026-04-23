48148f9f05e694d383a847d71d73770ee9dfc4f1
gabrielduprat <gabrielduprat@users.noreply.github.com>
2026-04-23 23:05:33 +0000

    edito para dispara

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.get("/", (req, res) => {
 app.get("/clients", (req, res) => {
     res.status(200).json({ message: "Ok", clients: ["uno","dos"]  });
 })
-
 app.get("/not_found", (req, res) => {
     res.status(404).json({ message: "Error not found" });
 })
